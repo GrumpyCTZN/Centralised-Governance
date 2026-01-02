b5d5c137d6826c5b7c43db788d1877bc6f521eec
GrumpyCTZN <abinashadhikari345@gmail.com>
2026-01-02 15:41:01 +0545

    Added new Search Page

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask,render_template, url_for
+from flask import Flask,render_template, url_for,request,redirect
 
 app=Flask(__name__)
 
-@app.route('/',)
+@app.route('/', methods=['POST','GET'])
 def index():
+    if request.method == 'POST':
+        searchQuery=request.form.get('searchQuery')
+        return redirect(url_for('handleSearch'))
     return render_template('index.html')
 
 @app.route('/commonservices')
@@ -13,6 +16,10 @@ def commonservices():
 @app.route('/officialwebsite')
 def officialWebsites():
     return render_template('officalwebsite.html')
+
+@app.route('/search')
+def handleSearch():
+    return render_template('search.html')
 
 if __name__=="__main__":
     app.run(debug=True)
