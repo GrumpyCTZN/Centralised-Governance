04372774ff4b783af7d257eee432e89a50367b9b
GrumpyCTZN <abinashadhikari345@gmail.com>
2026-01-02 13:39:14 +0545

    Routing Functions

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, url_for
 
 app=Flask(__name__)
 
@@ -6,5 +6,14 @@ app=Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/commonservices')
+def commonservices():
+    return render_template('commonservices.html')
+
+@app.route('/officialwebsite')
+def officialWebsites():
+    return render_template('officalwebsite.html')
+
 if __name__=="__main__":
     app.run(debug=True)
+    
