53b428fc670d7c371d80d12c6571ae1024e2dfdf
GrumpyCTZN <abinashadhikari345@gmail.com>
2026-01-02 16:17:50 +0545

    Re-Commit

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask,render_template, url_for,request,redirect
+from flask import Flask,render_template, url_for,request,redirect,session,abort
 
 app=Flask(__name__)
+app.secret_key='6...7'
 
 @app.route('/', methods=['POST','GET'])
 def index():
     if request.method == 'POST':
         searchQuery=request.form.get('searchQuery')
+        session['searchRequest']=True
         return redirect(url_for('handleSearch'))
     return render_template('index.html')
 
@@ -19,6 +21,8 @@ def officialWebsites():
 
 @app.route('/search')
 def handleSearch():
+    if not session.get('searchRequest'):
+        abort(403)
     return render_template('search.html')
 
 if __name__=="__main__":
