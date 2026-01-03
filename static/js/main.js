4408e7c18c40036109b83ad54867f271d7062be7
AnupCh37 <anup37@icloud.com>
2026-01-03 15:27:38 +0545

    type write

---
+++ static/js/main.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+  new Typed(".auto-type", {
+    strings: [
+      "Made Simple",
+      "for Every Citizen"
+    ],
+    typeSpeed: 80,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true
+  });
+});
